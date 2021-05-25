1e94f5d0008aa05a3d8bd8ba8140fe0371ca4ba2
chkim17 <79945536+chkim17@users.noreply.github.com>
2021-05-25 20:50:20 +0900

    v210525

---
+++ index.js
@@ -0,0 +1,9 @@
+const age = prompt("How old are you");
+
+if (age >= 18 && age <= 21) {
+  console.log("you can drink but you should not");
+} else if (age > 21) {
+  console.log("go ahed");
+} else {
+  console.log("too young");
+}
